7e6e33183b11230f16545982b1734e48ae7a8127
amr <amr.eldessouki@tactful.ai>
2022-01-21 17:54:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const rockImg = document.getElementById('rock');
+const papImg = document.getElementById('pap');
+const secImg = document.getElementById('sec');
+const userR = document.getElementById('yourR')
+const computerR = document.getElementById('computerR')
+const currentPlay = document.getElementById('result')
+// 1 3ayz a3rf el user a5tar eh
+// 2 5ly el comupter y5tar 7aga mn (rock papper scissor)
+// 3 3yzeen condition n3rf fe meen yksb el computer a5taro wla el user
+// 4 ntla3 (total) el nateg bta3 el condition ya computer ksb ya user ksb ya at3dlo
+// 5 hntl3 (each one) el natg l kol wa7ed ya3ny user : 1 computer : 0 
+userR.innerHTML = 1
+computerR.innerHTML = 1
+currentPlay.innerHTML = 'you win'
